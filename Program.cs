ba03513adaa293bf51920358aa3ca4714c06851a
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:18:52 +0530

    [Aniket] UC-3- valid Email id added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC3_ValidEmailID
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC3 patterns = new PatternsUC3();
+            Console.WriteLine("Enter Valid Email ID");
+            string email = Console.ReadLine();
+            if (patterns.validateString(email))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
