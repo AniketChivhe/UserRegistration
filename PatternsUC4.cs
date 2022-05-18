1d77b2e6bd63a4a363b2031b736ae62055c5e697
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:20:24 +0530

    [Aniket] UC-4- valid Phone No.

---
+++ PatternsUC4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC4_ValidPhoneNumber
+{
+    internal class PatternsUC4
+    {
+        // Applying Regex Rule
+        public static string RegexRules = @"^[0-9]+[\s]+[0-9]{10}$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC4_ValidPhoneNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC4 patterns = new PatternsUC4();
+            Console.WriteLine("Enter Valid Phone number with country code");
+            string phoneNo = Console.ReadLine();
+            if (patterns.validateString(phoneNo))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
