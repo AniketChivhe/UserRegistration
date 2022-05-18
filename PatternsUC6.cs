c3ee2b068f4e64f97ffcaf11de36dc145acf025d
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:24:13 +0530

    [Aniket] UC-5- valid Password upper case added

---
+++ PatternsUC6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6_PasswordUpperCase
+{
+    internal class PatternsUC6
+    {
+        // Applying Regex Rule
+        public static string RegexRules = @"^(?=.*[A-Z]).{8,}$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace UC6_PasswordUpperCase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC6 patterns = new PatternsUC6();
+            Console.WriteLine("Enter password minimum 8 characters with one upper case");
+            string password = Console.ReadLine();
+            if (patterns.validateString(password))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
