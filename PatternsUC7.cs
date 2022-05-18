6e3e013b459f8aa0c066c9133e1a7b311bc7dfa9
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:25:45 +0530

    [Aniket] UC-5- valid Password At least one digit added

---
+++ PatternsUC7.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC7_AtLeastOneDigit
+{
+    internal class PatternsUC7
+    {
+        // Applying Regex Rule
+        public static string RegexRules = @"^(?=.*[A-Z]).{8,}[0-9]$";   
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+    
+using System;
+
+namespace UC7_AtLeastOneDigit
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC7 patterns = new PatternsUC7();
+            Console.WriteLine("Enter password minimum 8 characters with one upper case and one numeric number");
+            string password = Console.ReadLine();
+            if (patterns.validateString(password))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
