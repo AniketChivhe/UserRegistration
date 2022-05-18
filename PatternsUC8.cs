8a791b14bc36194ec5034e1d3b380ee243c7014c
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:28:55 +0530

    [Aniket] UC-5- valid Password one spacial char added

---
+++ PatternsUC8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8_OneSpecialChar
+{
+    internal class PatternsUC8
+    {
+        // Applying Regex Rule
+        public static string RegexRules = @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&:;<>,.?~_+-=(){}|]).{8,}$";
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+namespace UC8_OneSpecialChar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC8 patterns = new PatternsUC8();
+            Console.WriteLine("Enter password minimum 8 characters with one upper case and one numeric number and special charactor");
+            string password = Console.ReadLine();
+            if (patterns.validateString(password))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
