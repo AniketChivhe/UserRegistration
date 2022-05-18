875fd82cb19a0ad231fb3c125f05168d04bd6db3
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:15:39 +0530

    [Aniket] UC-1 added

---
+++ PatternsUC1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC1_ValidFirstName
+{
+    internal class PatternsUC1
+    {
+        // Applying Regex Rule
+        public static string RegexRules = "^[A-Za-z]{2,}$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC1_ValidFirstName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC1 patterns = new PatternsUC1();
+            Console.WriteLine("Enter Valid First Name");
+            string firstname = Console.ReadLine();
+            if (patterns.validateString(firstname))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
