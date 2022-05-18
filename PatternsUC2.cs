266e8389cf8b6f332c7422d0b57248cda4e32a16
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:17:18 +0530

    [Aniket] UC-2 added

---
+++ PatternsUC2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2_ValidLastName
+{
+    internal class PatternsUC2
+    {
+        // Applying Regex Rule
+        public static string RegexRules = "^[A-Za-z]{2,}$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UC2_ValidLastName
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC2 patterns = new PatternsUC2();
+            Console.WriteLine("Enter Valid Last Name ");
+            string lastname = Console.ReadLine();
+
+            if (patterns.validateString(lastname))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
