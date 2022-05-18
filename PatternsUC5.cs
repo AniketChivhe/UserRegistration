afe92394accc66e186d5a9f620939c6b8f485f50
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-19 03:21:42 +0530

    [Aniket] UC-5- valid Password added

---
+++ PatternsUC5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC5_ValidPassword
+{
+    internal class PatternsUC5
+    {
+        // Applying Regex Rule
+        public static string RegexRules = @"[a-z,A-Z,0-9]{8,}$";
+        
+        public bool validateString(string sample)
+        {
+            return Regex.IsMatch(sample, RegexRules);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace UC5_ValidPassword
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PatternsUC5 patterns = new PatternsUC5();
+            Console.WriteLine("Enter password minimum 8 characters");
+            string password = Console.ReadLine();
+            if (patterns.validateString(password))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
