90f11bd0c9f45999bea978db8222d061296d521a
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-29 00:42:50 +0530

    [Aniket]UC9 for Validate email check with Unit test

---
+++ EmailTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UC9_EmailCheck
+{
+    public class EmailTest
+    {
+        public static string ValidatingEmailId(string email)
+        {
+            string RegexRules = (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");   
+            bool result = Regex.IsMatch(email, RegexRules);
+            if (result)
+            {
+                Console.WriteLine(email + " ----->Valid");
+                return email;
+            }
+            else
+            {
+                Console.WriteLine(email + " ----->Invalid");
+            }
+            return default;
+        }
+    }
+}
