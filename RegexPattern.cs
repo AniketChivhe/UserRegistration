8c196812621a17ecf43cc95900993bec8cb5c3f2
“AniketChivhe” <aniketchivhe@gmail.com>
2022-06-02 01:22:57 +0530

    [Aniket]UC10 for unit test added

---
+++ RegexPattern.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC10_UnitTest
+{
+    public class RegexPattern
+    {
+        public void Validating()
+        {       
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Validating The First Name");
+            Console.WriteLine("Enter the first name : ");
+            string input = Console.ReadLine();
+            ValidatingFirstName(input);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Validating The Last Name");
+            Console.WriteLine("Enter the last name : ");
+            string input1 = Console.ReadLine();
+            ValidatingLastName(input1);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Validating The Email Id");
+            Console.WriteLine("Enter Email Id");
+            string input2 = Console.ReadLine();
+            ValidatingEmailId(input2);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Validating The Phone Number with contry Code");
+            Console.WriteLine("Enter the phone number");
+            string input3 = Console.ReadLine();
+            ValidatingPhoneNum(input3);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("Validating The Password");
+            Console.WriteLine("Enter the password");
+            string input4 = Console.ReadLine();
+            ValidatingPassWord(input4);
+
+        }
+
+        public static string ValidatingFirstName(string firstName)
+        {
+            string RegexRules = "^[A-Za-z]{3,}$";
+            bool res = Regex.IsMatch(firstName, RegexRules);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+                return firstName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            return default;
+        }
+        public static string ValidatingLastName(string lastName)
+        {
+            string RegexRules = "^[A-Za-z]{3,}$";  
+            bool res1 = Regex.IsMatch(lastName, RegexRules);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+                return lastName;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            return default;
+        }
+        public static string ValidatingEmailId(string email)
+        {
+            string RegexRules = (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+            bool result = Regex.IsMatch(email, RegexRules);
+            if (result)
+            {
+                Console.WriteLine(email + " ----->Valid");
+                return email;
+            }
+            else
+            {
+                Console.WriteLine(email + " ----->Invalid");
+            }
+            return default;
+        }
+
+        public static string ValidatingPhoneNum(string phoneNum)
+        {
+           // string[] phoneNumInput = { "91 7852234896", " 91 9865741548", "919865795312", "91@123", "A865" };
+            string RegexRules = "^[0-9]+[ ]+[0-9]{10}$";
+
+            bool result = Regex.IsMatch(phoneNum, RegexRules);
+            if (result)
+            {
+                Console.WriteLine(phoneNum + " ----->Valid");
+                return phoneNum;
+            }
+            else
+            {
+                Console.WriteLine(phoneNum + " ----->InValid");
+            }
+            return default;
+        }
+        public static string ValidatingPassWord(string password)
+        {
+            //string[] passwordInput = { "Shalini@12", "sakAthi_32", "raksha123", "prathee" };
+            string RegexRules = @"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&:;<>,.?~_+-=(){}|]).{8,}$";
+
+            bool result = Regex.IsMatch(password, RegexRules);
+            if (result)
+            {
+                Console.WriteLine(password + " ----->Valid");
+                return password;
+            }
+            else
+            {
+                Console.WriteLine(password + " ----->InValid");
+            }
+            return default;
+        }
+    }
+}
---
+++ UnitTest_UC10.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UC10_UnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest_UC10
+    {
+        //validation for first Name
+        [TestMethod]    
+        [DataRow("Aniket", "Aniket")]
+        [DataRow("Ani", "Ani")]
+        [DataRow("A", null)]
+        [DataRow("Aniket07", null)]
+        public void ValidateFirstname(string a, string expected)
+        {
+            var actual = RegexPattern.ValidatingFirstName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validating for Last Name
+        [TestMethod]
+        [DataRow("Chivhe", "Chivhe")]
+        [DataRow("CH", null)]
+        [DataRow("CHIVHE", "CHIVHE")]
+        [DataRow("Chivhe77", null)]
+        public void ValidateUserLastname(string a, string expected)
+        {
+            var actual = RegexPattern.ValidatingLastName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validation for Email
+        [TestMethod]
+        [DataRow("abc@gmail.com", "abc@gmail.com")]
+        [DataRow("abc-100@yahoo.com", "abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com", "abc.100@yahoo.com")]
+        [DataRow("abc111@yahoo.com", "abc111@yahoo.com")]
+        [DataRow("abc111@abc.com", "abc111@abc.com")]
+        [DataRow("abc-100@abc.net", "abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au", "abc.100@abc.com.au")]
+        [DataRow("abc@1.com", "abc@1.com")]
+        [DataRow("abc@gmail.com.com", "abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com", "abc+100@gmail.com")]
+        [DataRow("abc", null)]
+        [DataRow("abc@.com.my", null)]
+        [DataRow("abc123@.com", null)]
+        [DataRow("abc123@.com.com", null)]
+        [DataRow("abc()*@gmail.com", null)]
+        [DataRow(".abc@abc.com", null)]
+        [DataRow("abc@%*.com", null)]
+        [DataRow("abc..2002@gmail.com", null)]
+        [DataRow("abc.@gmail.com", null)]
+        [DataRow("abc@abc@gmail.com", null)]
+        [DataRow("abc@gmail.com.1a", null)]
+        [DataRow("abc@gmail.com.aa.au", null)]
+        public void ValidateUserEmail(string a, string expected)
+        {
+            var actual = RegexPattern.ValidatingEmailId(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validation for Phone Number
+        [TestMethod]
+        [DataRow("1 1000984562", "1 1000984562")]
+        [DataRow("91 9845656232", "91 9845656232")]
+        [DataRow("100 9845656232", "100 9845656232")]
+        [DataRow("919845656232", null)]
+        [DataRow("91986523", null)]
+        [DataRow("91 844655", null)]
+        [DataRow("91 844566222000", null)]
+        public void ValidateUserPhoneNumber(string a, string expected)
+        {
+            var actual = RegexPattern.ValidatingPhoneNum(a);
+            Assert.AreEqual(expected, actual);
+        }
+        ////Validation for Password
+        [TestMethod]
+        [DataRow("Aniket@123", "Aniket@123")]
+        [DataRow("IamRandom#123", "IamRandom#123")]
+        [DataRow("aNiket12345", "aNiket12345")]
+        [DataRow("@Ani0712", "@Ani0712")]
+        [DataRow("Aniket7777", "Aniket7777")]
+        [DataRow("ANiKet", null)]
+        [DataRow("PASSword", null)]
+        [DataRow("pAsSwOrD", null)]
+        [DataRow("Aniket", null)]
+        [DataRow("XYZ(:123", null)]
+        public void ValidateUserPassword(string a, string expected)
+        {
+            var actual = RegexPattern.ValidatingPassWord(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
